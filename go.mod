f3c7fdb732ad18e5a32997ec189deffe4ea0db4d
jobala <japhethobalak@gmail.com>
2024-06-28 08:59:33 +0300

    add log messages

--- go.mod
+++ go.mod
@@ -1,10 +1,7 @@
 module github.com/jobala/handaki
 
-go 1.22.3
+go 1.19
 
-require (
-	github.com/songgao/water v0.0.0-20200317203138-2b4b6d7c09d8
-	golang.org/x/net v0.26.0
-)
+require github.com/songgao/water v0.0.0-20200317203138-2b4b6d7c09d8
 
 require golang.org/x/sys v0.21.0 // indirect
--- main.go
+++ main.go
@@ -16,12 +16,12 @@ func main() {
 
 	tun, err := water.New(water.Config{DeviceType: water.TUN})
 	if err != nil {
-		log.Fatal("Failed to open tun device")
+		log.Fatalf("Failed to open tun device: %v", err)
 	}
 
 	if isServer {
 		server.Start(tun, ":4000")
 	} else {
-		client.Start(tun, "")
+		client.Start(tun, "<Your Server>")
 	}
 }
